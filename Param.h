e31df7c6f3fbb76017248551a92b220f025ec684
FeeLIN <Robot2100@mail.ru>
2017-08-01 15:34:57 +0300

    Changed Param class

--- Param.h
+++ Param.h
@@ -5,8 +5,9 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 #include "Error.h"
-class Param
+class OldParam
 {
 protected:
 	std::vector<bool> found;
@@ -20,7 +21,7 @@ protected:
 	bool noname = false;
 	std::string nonamePar;
 public:
-	Param() {}
+	OldParam() {}
 	void AddNoname(std::string params)
 	{
 		nonamePar = params;
@@ -47,12 +48,7 @@ public:
 					nonamestr.push_back(argv[i]);
 				}
 				else {
-//					std::cout << "Not found '-' before parameter" << std::endl;
-//					std::cout << "Program terminated!" << std::endl;
-//					system("pause");
-//					exit(-1);
 					MyError::StopFatalError("Didn't find \"-\" before parameter");
-
 				}
 			}
 			else
@@ -71,11 +67,6 @@ public:
 						}
 					}
 				}
-				//not found in list
-//				std::cout << "Unknown parameter: " << argv[i] << std::endl;
-//				std::cout << "Program terminated!" << std::endl;
-//				system("pause");
-//				exit(-1);
 				MyError::StopFatalError("Unknown parameter: " + std::string(argv[i]));
 			}
 			else {
@@ -86,20 +77,11 @@ public:
 		founded:
 			;
 		}
-					for (int i = 0; i < (int)found.size(); i++) {
+		for (int i = 0; i < (int)found.size(); i++) {
 			if (found[i] == false)
 				continue;
 			if (minargs[i] >(int)strout[i].size() || maxargs[i] < (int)strout[i].size()) {
-				std::cout << "Parameter ";
-				for (int j = 0; j < (int)names[i].size(); j++) {
-					std::cout << names[i][j];
-					if (j != names[i].size() - 1)
-						std::cout << "/";
-				}
-				std::cout << " has wrong number of args!" << std::endl;
-				std::cout << "Program terminated!" << std::endl;
-				system("pause");
-				exit(-1);
+				MyError::StopFatalError("Parameter has wrong number of args!");
 			}
 		}
 	}
@@ -137,5 +119,133 @@ public:
 	}
 };
 
+template <std::size_t K>
+class Param2;
+class BaseParam {
+protected:
+	const char * param;
+	const char * desc;
+public:
+	template<std::size_t N, std::size_t M>
+	constexpr BaseParam(const char(&p)[N], const char(&des)[M])
+		: param(p), desc(des) {}
+	constexpr BaseParam() : param(nullptr), desc(nullptr) {
+	}
+	template <std::size_t K>
+	friend class Param;
+};
+template <std::size_t N>
+class ConstParam {
+protected:
+	const BaseParam *par;
+
+public:
+	constexpr ConstParam(const BaseParam (&in) [N]) : par(in) {
+		static_assert(N != 0, "ConstParam<size_t N> : N mustn't be 0");
+	}
+	template <size_t K>
+	friend class Param;
+};
+template <std::size_t N>
+class Param {
+protected:
+	const ConstParam<N> * CP = nullptr;
+	bool found[N];
+	std::vector<std::string> strout[N];
+	int noname = -1;
+public:
+	Param(const ConstParam<N> & par) : CP(&par) {
+		for (int i = 0; i < N; i++) {
+			found[i] = false;
+			if (std::strcmp(par.par[i].param, "") == 0) {
+				if (debug) {
+					if (noname != -1) {
+						MyError::StopFatalError("2 or more noname Parameters");
+					}
+				}
+				noname = i;
+			}
+		}
+	}
+	void TakeAgrs(int argn, char * argv[], void(*func)(const int &, std::vector<std::string> &))
+	{
+		//int k = -1, l = -1;
+		if (argn < 2) return;
+		std::vector<std::string> res;
+		int i = 1;
+		do {
+			if (argv[i][0] != '-') {
+				if (noname != -1) {
+					res.push_back(argv[i]);
+				}
+				else {
+					if (debug)
+						MyError::StopFatalError("Didn't find \"-\" before parameter");
+					else
+						MyError::FatalError("Didn't find \"-\" before parameter");
+				}
+			}
+			else
+				break;
+			i++;
+		} while (i < argn);
+		if (i != 1) {
+			func(noname, res);
+			res.clear();
+		}
+		int last = -1;
+		for (; i < argn; i++) {
+			if (argv[i][0] == '-') {
+				//search for parameter
+				if (last != -1) {
+					func(last, res);
+					res.clear();
+					last = -1;
+				}
+				for (int k = 0; k < N; k++) {
+					if (std::strcmp((CP->par[k]).param, argv[i]) == 0) {
+						last = k;
+					}
+				}
+				if (last == -1) {
+					if (debug)
+						MyError::StopFatalError("Unknown parameter: " + std::string(argv[i]));
+					else
+						MyError::FatalError("Unknown parameter: " + std::string(argv[i]));
+				}
+			}
+			else {
+				//add new res
+				res.push_back(argv[i]);
+			}
+		}
+		if (last != -1) {
+			func(last, res);
+			res.clear();
+		}
+		help(res);
+		func(-1, res);
+	}
+private:
+	void help(std::vector<std::string> & out) const
+	{
+		std::vector<std::string> res;
+		stringstream sstemp;
+		if (noname!=-1) {
+			res.push_back("Basic input:");
+			sstemp << "    " << CP->par[noname].desc;
+			res.push_back(sstemp.str());
+			sstemp.str(std::string());
+		}
+		res.push_back("Parameters:");
+		for (int i = 0; i < N; i++) {
+			if (noname == i) continue;
+			sstemp << "    " << CP->par[i].param << "\t: " << CP->par[i].desc;
+			res.push_back(sstemp.str());
+			sstemp.str(std::string());
+		}
+		res.swap(out);
+	}
+};
 
 #endif
